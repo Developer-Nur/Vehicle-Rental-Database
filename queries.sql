394e2334c182b07970edff4f9f70c15281755ef4
developer-nur <nursweb6@gmail.com>
2025-12-23 17:43:55 +0600

    4th query completed

--- queries.sql
+++ queries.sql
@@ -1,48 +1,18 @@
--- users table
-CREATE TABLE users (
-  user_id serial PRIMARY KEY,
-  name varchar(20) NOT NULL,
-  email varchar(50) UNIQUE NOT NULL,
-  password text NOT NULL,
-  phone int,
-  role varchar(20) NOT NULL
-);
-
--- vehicle table
-CREATE TABLE vehicles (
-  vehicle_id serial PRIMARY KEY,
-  name varchar(50) NOT NULL,
-  type varchar(50) NOT NULL,
-  registration_number varchar(50) UNIQUE NOT NULL,
-  model varchar(30),
-  price_per_day int NOT NULL,
-  available_status varchar(30) DEFAULT 'available'
-);
-
--- bookings table
-CREATE TABLE bookings (
-  booking_id serial PRIMARY KEY,
-  user_id int REFERENCES users (user_id),
-  vehicle_id int REFERENCES vehicles (vehicle_id),
-  start_date date,
-  end_date date,
-  booking_status varchar(50),
-  total_cost int
-);
-
 -- Retrieve booking information along with Customer name and Vehicle name.
-select
+SELECT
   b.booking_id,
-  u.name as customer_name,
-  v.name as vehicle_name,
+  u.name AS customer_name,
+  v.name AS vehicle_name,
   b.start_date,
   b.end_date,
   b.booking_status
-from
-  bookings as b
-  inner join users as u on u.user_id = b.user_id
-  inner join vehicles as v on v.vehicle_id = b.vehicle_id
-  order by b.booking_id;
+FROM
+  bookings AS b
+  INNER JOIN users AS u ON u.user_id = b.user_id
+  INNER JOIN vehicles AS v ON v.vehicle_id = b.vehicle_id
+ORDER BY
+  b.booking_id;
+
 
 -- Find all vehicles that have never been booked.
 SELECT
@@ -67,11 +37,32 @@ WHERE
 ORDER BY
   vehicle_id;
 
+
 -- Retrieve all available vehicles of a specific type.
 SELECT
-  *
+  vehicle_id,
+  name,
+  type,
+  model,
+  registration_number,
+  price_per_day AS rental_price,
+  available_status AS status
 FROM
   vehicles
 WHERE
   available_status = 'available'
   AND type = 'car';
+
+
+-- Find the total number of bookings for each vehicle and display only those vehicles that have more than 2 bookings.
+SELECT
+  v.name AS vehicle_name,
+  count(*) AS total_bookings
+FROM
+  vehicles AS v
+  INNER JOIN bookings AS b ON v.vehicle_id = b.vehicle_id
+GROUP BY
+  v.name,
+  v.vehicle_id
+HAVING
+  count(*) > 2;
